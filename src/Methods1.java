e95d8993fb273ec257057a3c7f8f4f821ae000d5
Diana Ndinda Nyamai <DNNYAMAI@safaricom.co.ke>
2022-09-30 15:36:03 +0300

    first commit

---
+++ src/Methods1.java
@@ -0,0 +1,9 @@
+public class Methods1 {
+    public static void Greet(){
+        System.out.println("I am a girl");
+    }
+    public static void main(String[] args) {
+        Greet();
+        Greet();
+    }
+}
