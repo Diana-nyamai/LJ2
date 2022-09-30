438e5db9d4dd7ce5ccaa023778bb51c3c0a39951
Diana-nyamai <dnyamai.dn@gmail.com>
2022-09-30 17:06:03 +0300

    Mtd update

--- src/Methods1.java
+++
@@ -1,9 +0,0 @@
-public class Methods1 {
-    public static void Greet(){
-        System.out.println("I am a girl");
-    }
-    public static void main(String[] args) {
-        Greet();
-        Greet();
-    }
-}
---
+++ src/Mtd.java
@@ -0,0 +1,8 @@
+public class Mtd {
+    public static void Greet(String name){
+        System.out.println("Hello " + name);
+    }
+    public static void main(String[] args) {
+   Greet("Diana");
+    }
+}
