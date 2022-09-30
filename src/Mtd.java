7407ebcb8a18e83f343b6ce839c6bd4b8a644ec1
Diana-nyamai <dnyamai.dn@gmail.com>
2022-09-30 17:07:07 +0300

    Mtd update

--- src/Mtd.java
+++ src/Mtd.java
@@ -1,6 +1,6 @@
 public class Mtd {
     public static void Greet(String name){
-        System.out.println("Hello " + name);
+        System.out.println("Hellow " + name);
     }
     public static void main(String[] args) {
    Greet("Diana");
