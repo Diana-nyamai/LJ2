3d4403898af28bc080417edf8ed072d265b0fb21
Diana-nyamai <dnyamai.dn@gmail.com>
2022-10-05 10:41:25 +0300

    Array list java

---
+++ src/ArrayList1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+      ArrayList<String> cars = new ArrayList<String>();
+      cars.add("mercedes");
+      cars.add("axios");
+      cars.add("Ferrari");
+      cars.add("Prado");
+      cars.set(3, "tx");
+
+      String acc = cars.get(2);
+        System.out.println(acc);
+//        System.out.println(change);
+    }
+}
