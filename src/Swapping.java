780488b18b77a0929a36202ee2b44e36ab6d3b40
Diana-nyamai <dnyamai.dn@gmail.com>
2022-10-17 13:23:36 +0300

    swapping variables

---
+++ src/Swapping.java
@@ -0,0 +1,15 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int temp;
+        int a= 20;
+        int b = 30;
+
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("value a " + a);
+        System.out.println("value b " + b);
+
+
+    }
+}
