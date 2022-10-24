fe1436c4ae02b7017daeae8f700d6d01b91a03ea
Diana-nyamai <dnyamai.dn@gmail.com>
2022-10-24 19:28:34 +0300

    main file practice

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
       int x = 1243;
+      String  xi = "hello";
+        System.out.println(" The length of the string is "+ xi.length());
       String a = String.valueOf(x);
+      int y;
       for(int i=0; i<String.valueOf(x).length(); i++){
-          int y = Integer.parseInt(String.valueOf(a.charAt(i)));
+          y = Integer.parseInt(String.valueOf(a.charAt(i)));
           System.out.println(y);
       }
 
