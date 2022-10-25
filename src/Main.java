5c0c437211de580fd79ddabf290723fc455bc282
Diana-nyamai <dnyamai.dn@gmail.com>
2022-10-25 18:49:19 +0300

    reverse string

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-      int x = 1243;
-      String  xi = "hello";
-        System.out.println(" The length of the string is "+ xi.length());
-      String a = String.valueOf(x);
-      int y;
-      for(int i=0; i<String.valueOf(x).length(); i++){
-          y = Integer.parseInt(String.valueOf(a.charAt(i)));
-          System.out.println(y);
+      int a = 121%10;
+        System.out.println(a);
+
       }
 
     }
-}
