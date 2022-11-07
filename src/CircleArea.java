3f456dcdd7e48b7567ee7d413cfa76f1ac81b6e5
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-07 20:57:36 +0300

    CircleArea.java

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -1,8 +1,8 @@
 public class CircleArea {
-
+    static Operation op;
     static double pi = 3.14d;
     public static double area(int radius){
-        Operation op = new Operation();
+      op = new Operation();
         double rsquare = (pi * op.square(radius));
         return rsquare;
     }
