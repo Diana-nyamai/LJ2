b5e25a87d8fea811c472972317b8c4a9dafb68a5
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-08 09:50:13 +0300

    Maximum69 solution

--- src/leetcode/Maximum69.java
+++ src/leetcode/Maximum69.java
@@ -2,12 +2,9 @@ package leetcode;
 
 public class Maximum69 {
     public static int maximum69Number (int num) {
-       int size = String.valueOf(num).length();
-
-       for(int i=0; i<size; i++){
-           System.out.println(i);
-       }
-       return num;
+        String x = String.valueOf(num);
+        int y = Integer.parseInt(x.replaceFirst("6","9"));
+        return y;
     }
     public static void main(String[] args) {
         System.out.println(maximum69Number(9669));
