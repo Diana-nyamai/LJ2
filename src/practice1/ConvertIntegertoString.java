9bcf4b4bf293253ac00b1db76ced8cc79f1e6970
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-08 20:26:44 +0300

    ConvertIntegertoString.java

---
+++ src/practice1/ConvertIntegertoString.java
@@ -0,0 +1,28 @@
+package practice1;
+
+import java.text.DecimalFormat;
+
+public class ConvertIntegertoString {
+    public static void main(String[] args) {
+        int x = 123;
+//        using integer.toString()
+        String y = Integer.toString(123);
+//        using String.valueOOf()
+        String z = String.valueOf(x);
+//        usig decimal format
+        DecimalFormat df = new DecimalFormat();
+        System.out.println("decimal format " + df.format(x));
+//        using string buffer
+        StringBuffer sb = new StringBuffer();
+        sb.append(x);
+        System.out.println("string buffer "+sb.toString());
+
+//        using string builder
+        String sbuilder = new StringBuilder().append(x).toString();
+
+        System.out.println("String Builder "+sbuilder);
+
+        System.out.println("toString " + y);
+        System.out.println("valueof " + z);
+    }
+}
