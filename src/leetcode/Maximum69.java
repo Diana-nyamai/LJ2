51af90bf4e2f2b995c318813eeabb67107b26db0
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-08 09:07:02 +0300

    Maximum69.java

--- src/leetcode/Maximum69.java
+++ src/leetcode/Maximum69.java
@@ -1,4 +1,15 @@
 package leetcode;
 
 public class Maximum69 {
+    public static int maximum69Number (int num) {
+       int size = String.valueOf(num).length();
+
+       for(int i=0; i<size; i++){
+           System.out.println(i);
+       }
+       return num;
+    }
+    public static void main(String[] args) {
+        System.out.println(maximum69Number(9669));
+    }
 }
