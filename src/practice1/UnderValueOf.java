33baed8a51fda71d5157bc07b911bbca6f933d3d
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-08 09:06:35 +0300

    understanding valueof

---
+++ src/practice1/UnderValueOf.java
@@ -0,0 +1,8 @@
+package practice1;
+
+public class UnderValueOf {
+    public static void main(String[] args) {
+       Integer obj = new Integer(10);
+        System.out.println(Integer.valueOf(10));
+    }
+}
