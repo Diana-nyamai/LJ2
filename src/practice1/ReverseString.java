51ac078707c3c13410f16827add14238c6f80668
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-10 08:41:17 +0300

    ReverseString.java

--- src/practice1/ReverseString.java
+++ src/practice1/ReverseString.java
@@ -4,8 +4,11 @@ public class ReverseString {
     public static void main(String[] args) {
         String s = "DIANA";
         int end = s.length() - 1;
+        StringBuilder str = new StringBuilder();
         for(int i=end; i>=0; i--){
-            System.out.println(s.charAt(i));
+            char y = s.charAt(i);
+            str.append(y);
         }
+        System.out.println(str);
     }
 }
