4784e8149063277886e645fe39fa9362f49c1a55
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-11 06:28:23 +0300

    main update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-      int a = 121%10;
-        System.out.println(a);
-
-      }
-
+        System.out.println("hi");
+        char s = 'a';
+        char t = 'b';
+        if(s == t){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+    }
     }
