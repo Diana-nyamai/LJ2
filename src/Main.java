9f7ba97074ce848a5c3ccbe9b9cf3609766e665f
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-13 21:34:49 +0300

    main update

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
-        char s = 'a';
-        char t = 'b';
-        if(s == t){
-            System.out.println(true);
-        }
-        else {
-            System.out.println(false);
-        }
+        ArrayList<Integer> list = new ArrayList<>();
+//        list.add(20);
+//        list.add(30);
+//        System.out.println(list.get(1));
+        int n = -1;
+        for(int i=0; (i< list.size()&&list.get(i)<n); i++);
+
     }
     }
