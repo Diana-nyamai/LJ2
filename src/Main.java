6c25bf4e4f5a938a39f1f931f49e4b8deb2efb43
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-16 16:13:30 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         ArrayList<Integer> list = new ArrayList<>();
-//        list.add(20);
-//        list.add(30);
-//        System.out.println(list.get(1));
+        list.add(20);
+        list.add(30);
+        System.out.println(list.get(1));
         int n = -1;
         for(int i=0; (i< list.size()&&list.get(i)<n); i++);
 
