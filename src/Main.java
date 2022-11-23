1dd19bbe96a1a598d691ed7ba3056d03b4d10230
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-23 14:43:15 +0300

    main java

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(20);
-        list.add(30);
-        System.out.println(list.get(1));
-        int n = -1;
-        for(int i=0; (i< list.size()&&list.get(i)<n); i++);
+       int x = 11/3;
+       int y = (int) Math.sqrt(11);
+        System.out.println("sqrt: " + y);
+        System.out.println(x);
 
     }
     }
