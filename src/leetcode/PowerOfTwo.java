3e00a493a0b033a392a3e39ba0ec1091d48b0e40
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-28 15:21:17 +0300

    power of two

--- src/leetcode/PowerOfTwo.java
+++ src/leetcode/PowerOfTwo.java
@@ -1,16 +1,20 @@
 package leetcode;
-//solution passed the test
+//solution all passed the test
 public class PowerOfTwo {
     public static boolean isPowerOfTwo(int n) {
-        if(n == 1 || n%2 == 0){
-          return true;
+        if(n == 0){
+          return false;
         }
-        else {
+        else if(n == 1){
+            return true;
+        }
+        else if(n%2 != 0){
             return false;
         }
+        return isPowerOfTwo(n/2);
     }
 
     public static void main(String[] args) {
-        System.out.println(isPowerOfTwo(3));
+        System.out.println(isPowerOfTwo(6));
     }
 }
